ce894031e68a9f2f728426824109b7040aa0dc37
azen <aidenzich0221@gmail.com>
2022-12-25 18:14:58 +0800

    Add dbschema

--- README.md
+++ README.md
@@ -21,8 +21,15 @@ npx directus start
     ```
 - 5. Copy your builed file (`/dist/index.js`) to the directus project's `extenstions` folder
 - 6. Run `npx directus start` to test your custom endpoint.
-- 7. Done!
+- 7. `localhost:8055` Done!
 
+
+## Error handle
+- Check your node version
+    ```
+    npx directus init
+    ```
+- `npm start` doesn't actually install / migrate the database. It just starts the server. You can use `npx directus init` to install the DB
 
 ## Reference
 1. https://stackoverflow.com/questions/72380698/how-to-create-and-deploy-an-endpoint-with-directus
---
+++ schema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE patients (
+  patient_id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  gender CHAR(1) NOT NULL,
+  age INTEGER NOT NULL,
+  contact_info TEXT
+);
+
+CREATE TABLE doctors (
+  doctor_id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  gender CHAR(1) NOT NULL,
+  age INTEGER NOT NULL,
+  contact_info TEXT
+);
+
+CREATE TABLE medical_records (
+  record_id SERIAL PRIMARY KEY,
+  patient_id INTEGER REFERENCES patients (patient_id),
+  doctor_id INTEGER REFERENCES doctors (doctor_id),
+  diagnosis TEXT NOT NULL,
+  treatment_plan TEXT NOT NULL,
+  date_created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE prescriptions (
+  prescription_id SERIAL PRIMARY KEY,
+  patient_id INTEGER REFERENCES patients (patient_id),
+  doctor_id INTEGER REFERENCES doctors (doctor_id),
+  drug_name TEXT NOT NULL,
+  dosage REAL NOT NULL,
+  frequency TEXT NOT NULL,
+  date_created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE drug_stocks (
+  drug_id SERIAL PRIMARY KEY,
+  drug_name TEXT NOT NULL,
+  quantity INTEGER NOT NULL,
+  price REAL NOT NULL,
+  batch_number TEXT NOT NULL,
+  expiration_date DATE NOT NULL
+);
+
+CREATE TABLE sales (
+  sale_id SERIAL PRIMARY KEY,
+  drug_id INTEGER REFERENCES drug_stocks (drug_id),
+  quantity INTEGER NOT NULL,
+  price REAL NOT NULL,
+  total_amount REAL NOT NULL,
+  date_created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
